2dcb5ec3f70e066a8971a672c24db47cd08e252d
Code Forefront <jonathan@code-forefront.com>
2017-12-09 13:15:20 +0100

    first commit after generation

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import router from './router'
+import plugin from 'vue-website-plugins/vue'
+
+Vue.config.productionTip = false
+Vue.use(plugin, {router})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<router-view id="app"/>'
+})
